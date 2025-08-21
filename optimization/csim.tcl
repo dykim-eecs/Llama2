b3da9b771b48ff14f7b20392cdc5b521815cd6cb
dykim-eecs <dykim.eecs@gmail.com>
2025-08-21 17:57:40 +0900

    Create csim.tcl

---
+++ optimization/csim.tcl
@@ -0,0 +1,15 @@
+# run_hls.tcl
+open_project proj_rmsnorm
+set_top rmsnorm
+#add_files rmsnorm.cpp
+add_files -tb testbench.cpp
+
+open_solution "sol1"
+set_part {xcu250-figd2104-2L-e}
+create_clock -period 3.3 -name default
+
+csim_design    ;# C simulation
+#csynth_design  ;# HLS synthesis
+#cosim_design   ;# C/RTL co-simulation
+#export_design -format xo
+exit
